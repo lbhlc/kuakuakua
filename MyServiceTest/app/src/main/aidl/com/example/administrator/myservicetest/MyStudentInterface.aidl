12caaaa09a1f37828cf87e7ee010888ebce3eb9e
lbhlc <76681287@qq.com>
2017-10-25 09:53:44 +0800

    提交完毕

---
+++ MyServiceTest/app/src/main/aidl/com/example/administrator/myservicetest/MyStudentInterface.aidl
@@ -0,0 +1,9 @@
+// MyStudentInterface.aidl
+package com.example.administrator.myservicetest;
+
+// Declare any non-default types here with import statements
+
+interface MyStudentInterface {
+
+    String getStudentName(int num);
+}
---
+++ MyServiceTest/app/src/main/java/com/example/administrator/myservicetest/AidlStudentService.java
@@ -0,0 +1,43 @@
+package com.example.administrator.myservicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.annotation.Nullable;
+
+/**
+ * @author libohan
+ *         邮箱:76681287@qq.com
+ *         create on 2017/10/25.
+ */
+
+public class AidlStudentService extends Service{
+    String[] name=new String[]{"mao","jiang","deng"};
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new AidlBinder();
+    }
+    class AidlBinder extends MyStudentInterface.Stub
+    {
+
+        @Override
+        public String getStudentName(int num) throws RemoteException {
+
+            return getStudentNames(num);
+        }
+    }
+    private String getStudentNames(int num)
+    {
+        if (num>0&&num<4)
+        {
+            return name[num];
+        }else
+        {
+            return null;
+        }
+
+    }
+}
